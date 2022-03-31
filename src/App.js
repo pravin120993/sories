3767d3e2337b0a008b95dd5c6b4ded7fa3dba8d8
pravin kuimar <pravinit2@gmail.com>
2022-03-31 09:53:31 +0530

    base setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Stories from "react-insta-stories";
+import "./App.css";
+
+const stories = [
+  {
+    url: "https://impact-feed-revamp.s3.ap-south-1.amazonaws.com/Two-Takeaway-Habits-from-Successful--Leaders%21/feed_title/1648544606196pexels-gerd-altmann-21696.jpg",
+    duration: 5000,
+    header: {
+      heading: "Mohit Karekar",
+      subheading: "Posted 30m ago",
+      profileImage: "https://picsum.photos/100/100",
+    },
+  },
+  {
+    url: "https://impact-feed-revamp.s3.ap-south-1.amazonaws.com/Turmeric%2C-The-Solution-To-All-Your-Problems-/feed_title/1648041063130pexels-tamanna-rumee-7988009.jpg",
+    duration: 5000,
+    seeMore: ({ close }) => {
+      return (
+        <div
+          onClick={close}
+          style={{ width: "100%", height: "100%", background: "white" }}
+        >
+          Hello, click to close this.
+        </div>
+      );
+    },
+  },
+  {
+    url: "https://impact-feed-revamp.s3.ap-south-1.amazonaws.com//feed_title/1647853158573Untitled-1.jpg",
+  },
+  {
+    url: "https://impact-feed-revamp.s3.ap-south-1.amazonaws.com/Weight-Training-Vs-Cardio-/feed_title/1647436156786Untitled-1.jpg",
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Stories
+      stories={stories}
+      defaultInterval={4000}
+      width="100vw"
+      height="100vh"
+    />
   );
 }
 
