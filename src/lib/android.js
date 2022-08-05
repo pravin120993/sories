557f181d2a9e50bd22fd4a448898370477bc9342
Vikrant <vikipro1998@gmail.com>
2022-08-05 15:27:17 +0530

    android setup done

--- src/lib/android.js
+++ src/lib/android.js
@@ -1,10 +1,19 @@
 import { isAndroid } from "react-device-detect";
 
 export const openLink = (value) => {
-  console.log(value);
   try {
     if (isAndroid) {
       window.Android.openLink(value);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const controlSound = (value) => {
+  try {
+    if (isAndroid) {
+      window.Android.controlSound(value);
     }
   } catch (err) {
     console.log(err);
