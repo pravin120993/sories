505a181abd33e0706505f790be7a1ae610be0cc4
Vikrant <vikipro1998@gmail.com>
2022-08-19 14:13:14 +0530

    comments resolved

--- src/utility/dataUtility.js
+++ src/utility/dataUtility.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { openLink } from "../lib/android";
 class DataUtility {
+  static onSwipeUp = (toggleMore, data) => {
+    toggleMore(true);
+    openLink(data);
+  };
   static parseStoryData(data) {
     data.forEach((item) => {
       if (item.isSeeMore) {
         item["seeMoreCollapsed"] = ({ toggleMore, action }) => (
           <div
             className="customSeeMore box"
-            onClick={() => {
-              toggleMore(true);
-              openLink(item.seeMoreUrl);
-            }}
+            onClick={() => DataUtility.onSwipeUp(toggleMore, item.seeMoreUrl)}
           >
             <p className="mx-0 arrow1">^</p>
             <p className="mx-0 arrow2">^</p>
